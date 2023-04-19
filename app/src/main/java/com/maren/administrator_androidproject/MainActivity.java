3838657291d0e98590debb1f51fa4276c1ec8b93
Debu849 <200301120077@cutm.ac.in>
2023-04-19 19:55:24 +0530

    admin page

---
+++ app/src/main/java/com/maren/administrator_androidproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.maren.administrator_androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
