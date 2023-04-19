8c9940fe8d4e50dcadebd2b193f1431e6f4c3700
Shiba prasad Mallik <shibaprasad07vipul@gmail.com>
2023-04-19 22:15:41 +0530

    add all required file

---
+++ app/src/main/java/com/example/afterlogin/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.afterlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.afterlogin.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        }
+    ;}
+}
